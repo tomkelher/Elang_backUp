1066f0e764f694166de24d2306ee51633ed1908b
tomkelher <tom.kelher@student.uhasselt.be>
2016-10-21 15:08:29 +0200

    no message

--- les005/src/les005.erl
+++ les005/src/les005.erl
@@ -11,9 +11,10 @@
 
 %% API
 -export([testSuiteOef1/0,testSuiteOef2/0,testSuiteOef3/0]).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 -record(examen,{vak="", punt=0}).
 -record(student,{naam="", examens=[]}).
-
 %oef 1 -> studenten met minstens 3  onvoldoenden
 buizen(L) -> lists:filter(fun(#examen{vak=_,punt=P})-> P<10 end, L).
 testSuiteOef1() ->
@@ -28,7 +29,7 @@ minderGoedeStudenten(Studs) -> lists:filter(fun(#student{naam=_,examens = Exs })
 
 
 %oef 2 -> studenten die op alles geslaagd zijn
-  allesGeslaagd(Studs) ->  lists:filter(fun(#student{naam=_,examens = Exs}) ->
+allesGeslaagd(Studs) ->  lists:filter(fun(#student{naam=_,examens = Exs}) ->
   GeslaagdeVakken = lists:filter(fun(#examen{vak=_,punt = P}) -> P >=10 end,Exs),
   AantalGeslaagd = length(GeslaagdeVakken),
   TotaalAantal = length(Exs),
@@ -43,24 +44,12 @@ testSuiteOef2() ->
 
 
 %oef 3 Studenten met gewogen percentage <54 en 1 onvoldoende +  tussen 54 en 58 met 2 onvoldoendes.
-
 gewogenPerc(Studs) -> lists:filter(fun(#student{naam=_,examens = Exs}) ->
   GeslaagdeVakken = lists:filter(fun(#examen{vak=_,punt = P}) -> P >=10 end,Exs),
-  bepaalAlgoritme(GeslaagdeVakken,Exs) end, Studs).
-
+  ((gemiddeldeBer(Exs#examen.punt) < 54 andalso (length(Exs) - length(GeslaagdeVakken)) == 1) or
+  (gemiddeldeBer(Exs#examen.punt) < 58 andalso gemiddeldeBer(Exs#examen.punt) > 54 andalso (length(Exs) - length(GeslaagdeVakken)) == 2))
+end, Studs).
 gemiddeldeBer(Lijst) -> lists:sum(Lijst)/ length(Lijst).
-
-bepaalAlgoritme(Geslaagde,Alles) ->
-  Buizen = length(Alles) - length(Geslaagde);
-  if (gemiddeldeBer(Alles) < 54 andalsoBuizen == 1) -> false;
-    true->true
-    end;
-
-if (gemiddeldeBer(Alles) < 58 andalso gemiddeldeBer(Alles) > 54 andalso Buizen == 2) -> false;
-  true->true
-end;
-end.
-
 testSuiteOef3() ->
   Student = [
     #student{naam="Sander",examens=[#examen{vak="Wiskunde",punt=8},#examen{vak="B",punt=15},#examen{vak="C",punt=16},#examen{vak="D",punt=15}]},
@@ -68,3 +57,9 @@ testSuiteOef3() ->
     #student{naam="Gijs",examens=[#examen{vak="Fysica",punt=8},#examen{vak="B",punt=16},#examen{vak="C",punt=13},#examen{vak="D",punt=10}]}
   ],
   gewogenPerc(Student).
+
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+-record(examen,{vak="", punt=0}).
+-record(student,{naam="", examens=[]}).
+
---
+++ les005/src/les005V2.erl
@@ -0,0 +1,35 @@
+%%%-------------------------------------------------------------------
+%%% @author Eigenaar
+%%% @copyright (C) 2016, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 21. okt 2016 10:13
+%%%-------------------------------------------------------------------
+-module(les005V2).
+-author("Eigenaar").
+
+%% API
+-export([testSuiteOef1/0]).
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+-record(studenten,{naam=[]}).
+-record(vakken,{naam=[], punt=[]}).
+
+%oef 1 -> studenten met minstens 3
+
+
+
+telAantalBuizen({Student, Examens}) ->
+  {student, lists:foldl(fun(#vakken{naam=_,punt=P},Teller) ->
+    if P>= 10 -> Teller;true -> Teller+1 end
+  end,
+  0,
+  Examens}.
+
+%oef 2 -> studenten die op alles geslaagd zijn
+
+
+
+%oef 3 Studenten met gewogen percentage <54 en 1 onvoldoende +  tussen 54 en 58 met 2 onvoldoendes.
+
